09e2b4ef7e774e8d1900eb48ef1ef5a7ed4c58a1
Jin Gyun, Jeong <jjinggu96@gmail.com>
2020-09-13 20:46:17 +0900

    UPDATE : viewModel

---
+++ Example/season2/step3+Empty/RxSwift+MVVM/Pages/Menu.swift
@@ -0,0 +1,17 @@
+//
+//  Menu.swift
+//  RxSwift+MVVM
+//
+//  Created by JJIKKYU on 2020/09/13.
+//  Copyright © 2020 iamchiwon. All rights reserved.
+//
+
+import Foundation
+
+// Model : View를 위한 Model
+// ViewModel
+struct Menu {
+    var name : String
+    var price : Int
+    var count : Int
+}
---
+++ Example/season2/step3+Empty/RxSwift+MVVM/Pages/MenuListViewModel.swift
@@ -0,0 +1,19 @@
+//
+//  MenuListViewModel.swift
+//  RxSwift+MVVM
+//
+//  Created by JJIKKYU on 2020/09/13.
+//  Copyright © 2020 iamchiwon. All rights reserved.
+//
+
+import Foundation
+
+class MenuListViewModel {
+    
+    let menus : [Menu] = [
+        Menu(name: "튀김1", price: 100, count: 0),
+        Menu(name: "튀김2", price: 100, count: 0),
+        Menu(name: "튀김3", price: 100, count: 0),
+        Menu(name: "튀김4", price: 100, count: 0),
+    ]
+}
